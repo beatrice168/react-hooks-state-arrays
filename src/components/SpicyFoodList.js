f2ac7e0ef79f31678ea8f6a1956f12bd405fe219
beatrice <helgapaul389@gmail.com>
2023-04-21 10:55:10 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,24 +3,75 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
+  const [filterBy,setFilterBy]=useState("All");
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+    const FoodArray =  [...foods, newFood];
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  setFoods(FoodArray);
+    console.log(newFood);
+  } 
+  // return (
+  //   <div>
+  //     <button onClick={handleAddFood}>Add New Food</button>
+  //     <ul>{foodList}</ul>
+  //   </div>
+  // );
+  
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+       return true;
+     } else {
+       return food.cuisine === filterBy;
+     }
+   });
+
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+  
+  function handleLiClick(id){
+   //const FoodArray =foods.filter((food)=>food.id !== id)
+    //setFoods(FoodArray);
+    const FoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(FoodArray);
 
+  }
+  function handleFilterChange(event){
+   
+   
+    setFilterBy(event.target.value)
+  }
   return (
+    <React.Fragment>
+  <select name="filter" onChange={handleFilterChange}>
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>
+  
+
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
+    </React.Fragment>
   );
+ 
 }
 
 export default SpicyFoodList;
